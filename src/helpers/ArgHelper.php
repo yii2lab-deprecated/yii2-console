e53d777ea537775d4c8706e31d88ce1bf4e39313
Vitaliy <theyamshikov@yandex.ru>
2017-11-05 18:58:27 +0600

    renane helper

--- src/helpers/ArgHelper.php
+++ src/helpers/ArgHelper.php
@@ -6,7 +6,7 @@ use yii\helpers\ArrayHelper;
 
 // todo: move to console vendor
 
-class ParameterHelper {
+class ArgHelper {
 	
 	public static function one($name) {
 		$all = self::all();
@@ -14,16 +14,14 @@ class ParameterHelper {
 	}
 	
 	public static function all() {
-		$argv = [];
-		if (isset($_SERVER['argv'])) {
-			$argv = $_SERVER['argv'];
-			array_shift($argv);
-		}
+		$argv = self::getArgs();
+		return self::parse($argv);
+	}
+
+	private static function parse($argList) {
 		$result = [];
-		foreach($argv as $arg) {
-			$e = explode("=", $arg);
-			$name = $e[0];
-			$value = count($e) > 1 ? $e[1] : null;
+		foreach($argList as $arg) {
+			list($name, $value) = self::split($arg);
 			if(preg_match('/(.+)\[(.+)\]\[(.+)\]/', $name, $matches)) {
 				$result[ $matches[1] ] [ $matches[2] ] [ $matches[3] ] = $value;
 			} elseif(preg_match('/(.+)\[(.+)\]/', $name, $matches)) {
@@ -34,5 +32,20 @@ class ParameterHelper {
 		}
 		return $result;
 	}
-	
+
+	private static function split($arg) {
+		$e = explode("=", $arg);
+		$name = $e[0];
+		$value = count($e) > 1 ? $e[1] : null;
+		return [$name, $value];
+	}
+
+	private static function getArgs() {
+		$argv = [];
+		if (isset($_SERVER['argv'])) {
+			$argv = $_SERVER['argv'];
+			array_shift($argv);
+		}
+		return $argv;
+	}
 }
