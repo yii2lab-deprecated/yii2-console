e6941d0222485119bb673cc7ba5fc0c64351850e
Vitaliy <theyamshikov@yandex.ru>
2018-01-23 00:39:36 +0600

    make form input

--- src/helpers/input/Enter.php
+++ src/helpers/input/Enter.php
@@ -2,6 +2,9 @@
 
 namespace yii2lab\console\helpers\input;
 
+use Yii;
+use yii\base\Model;
+use yii\helpers\Inflector;
 use yii2lab\console\helpers\Output;
 
 class Enter {
@@ -16,10 +19,54 @@ class Enter {
 
 	static function arr($options) {
 		$result = [];
-		foreach($options as $name => $type) {
-			$result[$name] = self::display($name, $type);
+		foreach($options as $message => $type) {
+			$result[$message] = self::display(Inflector::titleize($message), $type);
 		}
 		return $result;
 	}
-
+	
+	static function form($form, $data = null, $scenario = null) {
+		/** @var Model $form */
+		if(!is_object($form)) {
+			$form = Yii::createObject($form);
+		}
+		if($data) {
+			$form->setAttributes($data);
+		}
+		if($scenario) {
+			$form->scenario = $scenario;
+		}
+		self::inputAll($form);
+		return $form->toArray();
+	}
+	
+	private static function inputAll(Model $form) {
+		$only = [];
+		do {
+			self::formInput($form, $only);
+			$isValidate = $form->validate();
+			if(!$isValidate) {
+				Output::arr($form->getFirstErrors(), 'Validation error');
+				$only = array_keys($form->getErrors());
+			}
+		} while(!$isValidate);
+	}
+	
+	private static function formInput(Model $form, $only = null) {
+		$attributeLabels = $form->attributeLabels();
+		foreach($form->attributes as $attributeName => $attributeValue) {
+			if(!empty($only) && !in_array($attributeName, $only)) {
+				continue;
+			}
+			$message = $attributeLabels[$attributeName];
+			if(!empty($attributeValue) && empty($only)) {
+				$message .= ' (default: ' . $attributeValue . ')';
+			}
+			$value = Enter::display($message);
+			if(!empty($value)) {
+				$form->{$attributeName} = $value;
+			}
+		}
+	}
+	
 }
