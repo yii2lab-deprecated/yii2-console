2be320476be088539983c29cd8afdb0fa5d44099
Vitaliy <theyamshikov@yandex.ru>
2018-01-28 22:52:24 +0600

    update form widget

--- src/helpers/input/Enter.php
+++ src/helpers/input/Enter.php
@@ -4,12 +4,11 @@ namespace yii2lab\console\helpers\input;
 
 use Yii;
 use yii\base\Model;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Inflector;
 use yii2lab\console\helpers\Output;
 
 class Enter {
-	
-	const ERROR = '{[$ERROR$]}';
 	
 	static function display($message, $type = 'string') {
 		echo PHP_EOL . $message . ': ';
@@ -27,11 +26,12 @@ class Enter {
 	
 	static function form($form, $data = null, $scenario = null) {
 		/** @var Model $form */
+		
 		if(!is_object($form)) {
 			$form = Yii::createObject($form);
 		}
 		if($data) {
-			$form->setAttributes($data);
+			Yii::configure($form, $data);
 		}
 		if($scenario) {
 			$form->scenario = $scenario;
@@ -62,11 +62,61 @@ class Enter {
 			if(!empty($attributeValue) && empty($only)) {
 				$message .= ' (default: ' . $attributeValue . ')';
 			}
-			$value = Enter::display($message);
-			if(!empty($value)) {
+			$rules = $form->rules();
+			$attributeType = self::getType($rules, $attributeName);
+			
+			if($attributeType['type'] == 'string') {
+				$value = Enter::display($message);
+			} elseif($attributeType['type'] == 'enum') {
+				if(ArrayHelper::isIndexed($attributeType['range'])) {
+					$value = Select::display($message, $attributeType['range']);
+					$key = key($value);
+					$value = $value[$key];
+				} else {
+					$value = Question::display($message, array_values($attributeType['range']));
+					$value = ArrayHelper::getValue($attributeType, "range.{$value}");
+				}
+			} elseif($attributeType['type'] == 'boolean') {
+				$value = Question::confirm($message);
+			}
+			if(!empty($value) || $value === '0' || $value === 0 || $value === false) {
 				$form->{$attributeName} = $value;
 			}
 		}
+	}
+	
+	private static function getType($rules, $attribute) {
+		$result = [
+			'type' => 'string',
+		];
+		foreach($rules as $rule) {
+			if(self::hasAttribute($rule, $attribute)) {
+				if($rule[1] == 'in') {
+					$range = $rule['range'];
+					$uc = [];
+					foreach($range as $item) {
+						$firstChar = $item{0};
+						$uc[$firstChar] = $item;
+					}
+					$range = count($uc) == count($range) ? $uc : $range;
+					$result = [
+						'type' => 'enum',
+						'range' => $range,
+					];
+				} elseif($rule[1] == 'boolean') {
+					$result = [
+						'type' => 'boolean',
+					];
+				}
+			}
+		}
+		return $result;
+	}
+	
+	private static function hasAttribute($rule, $attribute) {
+		$attributeList = $rule[0];
+		$attributeList = ArrayHelper::toArray($attributeList);
+		return in_array($attribute, $attributeList);
 	}
 	
 }
